b9f42259f1f2ebc1881bd6b12f70f97e8fae249c
KIM YONG KOOK <36836246+bluevlad@users.noreply.github.com>
2025-02-12 15:56:53 +0900

    delete

--- HAgnostic-News-master/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppContainer from './src/AppContainer'
-
-export default AppContainer;
--- HAgnostic-News-master/src/utils.js
+++
@@ -1,4 +0,0 @@
-export function domainUrl (url) {
-  const domain = url.split(/^https?:\/\/([^/?#]+)(?:[/?#]|$)/)[1]
-  return domain || 'n/a'
-}
